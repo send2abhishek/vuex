16847900027a88450be19118e9f44e8ddb3e0bf1
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-10-25 19:10:43 +0530

    create store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
+const store = createStore();
 const app = createApp(App);
 
+app.use(store);
 app.mount('#app');
